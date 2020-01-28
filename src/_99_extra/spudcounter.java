457970a0866578a5476a1ffcdb299d042856fc3a
ilabstaff <ilabstaff@172.16.116.128>
2020-01-27 18:17:39 -0800

    finish

---
+++ src/_99_extra/spudcounter.java
@@ -0,0 +1,22 @@
+package _99_extra;
+
+public class spudcounter {
+public static void main(String[] args) {
+for (int i = 1; i < 9; i++) {
+	if (i==4) {
+		System.out.println(4);
+	}
+	else if (i==8) {
+		System.out.println("more");
+	}
+	
+	else {
+		System.out.println(i+" potato");
+	}
+	
+	
+}
+		
+	}
+}
+
